edc03b0a339fb1d24cdd58ec19c81a4a071bec56
Alvin Lim <alvinlim@users.noreply.github.com>
2021-01-31 11:22:34 +0800

    Update
    Removed Draw() Function and added comments

--- analyze.py
+++ analyze.py
@@ -5,51 +5,24 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-
-def draw():
-    #Draw Rectangle on Face
-    faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    gray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(gray,1.1,4)
-    for(x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-
-
-    #Write Emotion on Image
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(img,
-               predictions['dominant_emotion'],
-               (400,200),
-               font, 2,
-               (0, 0, 255),
-               2,
-               cv2.LINE_4
-           ) ;
-
-
 def detectEmotion():
+    #Count total frames in folder
     list = os.listdir('Extracted Frames')
     number_files = len(list)
 
     for x in range(number_files):
-        #count
-
         #Analyze Image
         warnings.filterwarnings("ignore")
         img = cv2.imread('Extracted Frames/frame'+ str(x) +'.jpg')
         predictions = DeepFace.analyze(img)
         warnings.filterwarnings("ignore")
 
-        #draw
         #Draw Rectangle on Face
         faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
         gray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = faceCascade.detectMultiScale(gray,1.1,4)
         for(x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-
 
         #Write Emotion on Image
         font = cv2.FONT_HERSHEY_SIMPLEX
--- extract.py
+++ extract.py
@@ -1,27 +1,6 @@
 import cv2
 import os
 
-def draw():
-    #Draw Rectangle on Face
-    faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    gray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(gray,1.1,4)
-    for(x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-
-
-    #Write Emotion on Image
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(img,
-               predictions['dominant_emotion'],
-               (400,200),
-               font, 2,
-               (0, 0, 255),
-               2,
-               cv2.LINE_4
-           ) ;
-
 def extractFrames(n):
     path = n
     #Read video from specified path
